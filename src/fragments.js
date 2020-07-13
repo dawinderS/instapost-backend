43144c1620b85c1c47ef4fca84eb35aef59962e0
Dawinder Singh <dawindernsingh@gmail.com>
2020-07-13 01:59:00 -0700

    finish backend for seeing post

---
+++ src/fragments.js
@@ -0,0 +1,44 @@
+export const USER_FRAGMENT = `
+  id
+  username
+`;
+
+export const COMMENT_FRAGMENT = `
+  id
+  text
+  user {
+    username
+  }
+`;
+
+export const LIKE_FRAGMENT = `
+  id
+  user {
+    username
+  }
+`;
+
+export const FILE_FRAGMENT = `
+  id
+  url
+`;
+
+export const FULL_POST_FRAGMENT = `
+  fragment PostParts on Post{
+    id
+    location
+    caption
+    files {
+      ${FILE_FRAGMENT}
+    }
+    comments {
+      ${COMMENT_FRAGMENT}
+    }
+    likes {
+      ${LIKE_FRAGMENT}
+    }
+    user {
+      ${USER_FRAGMENT}
+    }
+  }
+`;
