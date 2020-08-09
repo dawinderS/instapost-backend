26cc3ec7dc7e44726dacf31b5455c9fa714ebb8a
Dawinder Singh <dawindernsingh@gmail.com>
2020-08-09 15:13:44 -0700

    add aws s3

---
+++ src/api/Post/seeExplore/seeExplore.js
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeExplore: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const following = await prisma.user({ id: user.id }).following();
+      const begin = await prisma.posts({
+        where: {
+          user: {
+            id_not_in: [...following.map((user) => user.id), user.id],
+          },
+        },
+        orderBy: "createdAt_DESC",
+      });
+      const end = await prisma.posts({
+        where: {
+          AND: [
+            {likes_none: {
+              user: {
+                id: user.id,
+              }
+            }},
+            {user: {
+              id_in: [...following.map((user) => user.id), user.id],
+            }},
+          ]
+        },
+        orderBy: "createdAt_DESC",
+      });
+      return begin.concat(end);
+    },
+  },
+};
+
